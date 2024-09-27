b453a002e13548e6336e20a44f9ca8a4270127d7
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-09-28 01:25:07 +0200

    added the super user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,9 @@
-from django.contrib import admin
 
 # Register your models here.
 
+from django.contrib import admin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['phone_number', 'is_staff', 'is_superuser']
--- accounts/models.py
+++ accounts/models.py
@@ -1,20 +1,39 @@
-
-
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, phone_number):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        """Create and return a user with an email and password."""
         if not phone_number:
-            raise ValueError("Users must have a phone number")
-        user = self.model(phone_number=phone_number)
+            raise ValueError("The Phone Number field must be set")
+
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)  # Hash the password
         user.save(using=self._db)
         return user
 
-class CustomUser(AbstractBaseUser):
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        """Create and return a superuser with an email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone_number, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(max_length=15, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
+    def __str__(self):
+        return self.phone_number
